f766ca4395b4bc129c0cb48dbad88e9359b4a179
Mauresong <41476956+Mauresong@users.noreply.github.com>
2018-08-02 22:33:20 +0800

    Create Addition.py

---
+++ Addition.py
@@ -0,0 +1,11 @@
+#prompt client enter numbers
+
+#get client's numbers(at least 2)
+num1 = int(input("Please enter your number a:"))
+num2 = int(input("Please enter your number a:"))
+
+
+#summing the 2 acquired numbers and outputting the corresponding result
+
+add = num1 + num2
+print('result is %d:'%(add))
